1d4d7972a535881e3d6a8e097cbf95ce30f56f4f
aaron <a.aa.akio.aaron@gmail.com>
2014-11-02 00:22:14 -0700

    Generic node captures

--- gen.js
+++ gen.js
@@ -41,8 +41,14 @@ transformer.register({
 
 // translate src
 
-patternSrc = 'rect($_x, $_y, $_width, $_height)'
-templateSrc = 'x:{{x.value}} y:{{y.value}} width:{{width.value}} height:{{height.value}}'
+// patternSrc = 'rect($_x, $_y, $_width, $_height)'
+// templateSrc = 'x:{{x.value}} y:{{y.value}} width:{{width.value}} height:{{height.value}}'
+// userSrc = 'rect(1,2,3,4)'
+
+patternSrc = 'console.log( $_greeting )'
+templateSrc = 'logging was found: {{greeting.type}}'
+userSrc = 'console.log( 1 + 2 ); console.log( "hay" );'
+
 templateRender = handlebars.compile(templateSrc)
 patternAst = esprima.parse(patternSrc)
 patternAst = patternAst.body[0]
@@ -70,15 +76,14 @@ tmpTrans.convertAst(patternAst, function(err, output){
   dump(patternAst)
   // use the code pattern as a rule
   transformer.register(patternAst, function(node, key, parent, matched, callback){
-    debugger
     callback(null, templateRender(matched))
   })
   // transform the user code using the new rule
-  ast = esprima.parse('rect(1,2,3,4)')
+  ast = esprima.parse(userSrc)
   console.log('USER CODE')
   dump(ast)
   transformer.convertAst(ast, function(){
-    console.log('render tree')
+    console.log('RENDER TREE')
     dump(ast)
   })
 })
